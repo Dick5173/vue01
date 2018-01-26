c40ef9127785805f1d3d3a4e27db3e1a399fe398
lintaiming <879512272@qq.com>
2018-01-26 11:22:56 +0800

    店铺页面列表展示

--- src/ui/other/tenant/Index.vue
+++ src/ui/other/tenant/Index.vue
@@ -2,19 +2,36 @@
   div
     div
       search-toolbar(:queryParams="queryParams", @submit="handleSearch")
-    div
-      el-table.list-el-table(:data="dataList.data", @sort-change="sortChanged", :defaultSort!='dataListSortInfo', border, @selection-change="handleSelectionChange")
+    div(v-loading="loading")
+      el-table.list-el-table(:data="dataList.data", @sort-change="sortChanged", :defaultSort!='dataListSortInfo', border)
         el-table-column(prop="id", label="店铺ID", width="65px")
-        el-table-column(prop="name", label="名称", width="")
-        el-table-column(prop="", label="销售额", sortable, width="")
-        el-table-column(prop="", label="店铺利润", sortable, width="")
-        el-table-column(prop="", label="上架商品", sortable, width="")
-        el-table-column(prop="", label="首次上线", sortable, width="")
+        el-table-column(prop="", label="名称", width="")
+          div(slot-scope="scope")
+            el-button(type="text", @click="toDetail(scope.row)") {{scope.row.nick_name}}
+        el-table-column(prop="total_amount", label="销售额", sortable, width="")
+          div(slot-scope="scope")
+            el-button(type="text", @click="toOrder(scope.row)") {{scope.row.total_amount  | price}}
+        el-table-column(prop="total_profit", label="店铺利润", sortable, width="")
+          div(slot-scope="scope") {{scope.row.total_profit  | price}}
+        el-table-column(prop="up_product_count", label="上架商品", sortable, width="")
+          div(slot-scope="scope")
+            el-button(type="text", @click="toProduct(scope.row)") {{scope.row.up_product_count}}
+        el-table-column(prop="first_uptime", label="首次上线", sortable, width="")
+          div(slot-scope="scope") {{scope.row.first_uptime | datetime}}
         el-table-column(prop="", label="小程序状态", width="")
+          div(slot-scope="scope") {{showAppStatus(scope.row.app_status)}}
         el-table-column(prop="", label="店铺管理员ID", width="")
+          div(slot-scope="scope") {{scope.row.admin_id}}
         el-table-column(prop="", label="退款授权", width="")
+          div(slot-scope="scope")
+            div(v-if="scope.row.refund_status === 2") 已授权
+            el-button(v-else, type="primary", size="small", @click="") 确认授权
         el-table-column(prop="", label="店铺状态", width="")
+          div(slot-scope="scope") {{showTenantStatus(scope.row.tenant_status)}}
         el-table-column(prop="", label="操作", width="", fixed="right")
+          div(slot-scope="scope")
+            el-button(v-if="scope.row.tenant_status === 1", type="danger", size="small", @click="") 禁用
+            el-button(v-else, type="primary", size="small", @click="") 启用
     el-pagination(:currentPage="queryPager.page", :pageSize="queryPager.limit", :total="dataListTotal",  @current-change="changePage")
 
 </template>
@@ -35,10 +52,10 @@
     data () {
       return {
         queryParams: {
-          status: '',
+          app_status: '',
           start: 0,
           end: 0,
-          text: ''
+          key: ''
         }
       }
     },
@@ -57,8 +74,32 @@
           return val
         })(params))
       },
-      handleSelectionChange (val) {
-        this.multipleSelection = val
+      showAppStatus (row) {
+        let text = ''
+        switch (row) {
+          case 1:
+            text = '审核中'
+            break
+          case 2:
+            text = '已上线'
+            break
+          case 3:
+            text = '已下线'
+            break
+          case 4:
+            text = '解除授权'
+            break
+        }
+        return text
+      },
+      showTenantStatus (row) {
+        if (row === 1) {
+          return '正常'
+        }
+        return '禁用'
+      },
+      toDetail (row) {
+
       },
       handleSearch (data) {
         this.queryChange(data)
--- src/ui/other/tenant/SearchToolBar.vue
+++ src/ui/other/tenant/SearchToolBar.vue
@@ -2,12 +2,12 @@
   div
     el-form(:inline="true")
       el-form-item(label="状态：")
-        el-select(v-model="formData.status", clearable)
+        el-select(v-model="formData.app_status", clearable)
           el-option(v-for="item in statusList", :key="item.status", :label="item.name", :value="item.status")
       el-form-item(label="首次上线：")
         date-picker(:defaultDate="defaultDate", @change = "changeDate")
       el-form-item
-        el-input(v-model.trim="formData.text", placeholder="店铺名称/店铺ID")
+        el-input(v-model.trim="formData.key", placeholder="店铺名称/店铺ID")
       el-form-item
         el-button(type="primary", icon="el-icon-search", @click="handleSearch") 搜&nbsp索
 </template>
@@ -23,17 +23,17 @@
     data () {
       return {
         formData: {
-          status: '',
+          app_status: '',
           start: 0,
           end: 0,
-          text: ''
+          key: ''
         },
         defaultDate: [],
         statusList: [
-          {name: '审核中', status: '0'},
-          {name: '已上线', status: '1'},
-          {name: '已下线', status: '2'},
-          {name: '解除授权', status: '3'}
+          {name: '审核中', status: '1'},
+          {name: '已上线', status: '2'},
+          {name: '已下线', status: '3'},
+          {name: '解除授权', status: '4'}
         ]
       }
     },
