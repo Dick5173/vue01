2aae217704aa78d2cd67c5106e0e83c2243f571e
lintaiming <879512272@qq.com>
2018-02-24 18:53:29 +0800

    添加权限管理页面ui

---
+++ src/api/authorization-management.js
@@ -0,0 +1,29 @@
+import Axios from 'axios'
+// import qs from 'qs'
+
+export const getAuthGroupList = (tp) => {
+  return Axios.get(`/admin/auth_group`, {
+    params: {
+      tp: tp
+    }
+  })
+}
+export const getAuthList = (tp) => {
+  return Axios.get(`/admin/auth`, {
+    params: {
+      tp: tp
+    }
+  })
+}
+export const createAuthGroup = (formData) => {
+  return Axios.post(`/admin/auth_group`, formData)
+}
+export const editAuthGroup = (formData) => {
+  return Axios.post(`/admin/auth_group/s/${formData.id}`, formData)
+}
+export const deleteAuthGroup = (id) => {
+  return Axios.delete(`/admin/auth_group/s/${id}`)
+}
+export const getAuthitem = (id) => {
+  return Axios.get(`/admin/auth/s/${id}`)
+}
---
+++ src/ui/authorization-management/AuthDialog.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  el-dialog(:title="showTitle()", :visible.sync="dialogVisible", @close="handleClose()")
+  el-form(ref="form", :model="formData", :rules="formRules", labelWidth="120px")
+    el-form-item(label="名称：", prop="name")
+      el-input.tiny-x-el-input(v-model="formData.name", :maxlength="10")
+    el-form-item(label="是否显示名称：", prop="show_name")
+      el-radio(v-model="formData.show_name", :label="true") 是
+      el-radio(v-model="formData.show_name", :label="false") 否
+  span(slot="footer" class="dialog-footer")
+    el-button(@click="dialogVisible = false") 取 消
+    el-button(type="primary", @click="submit", v-loading="loading") 确 定
+</template>
+
+<script>
+  export default {
+    props: {},
+    components: {},
+    data () {
+      return {}
+    },
+    computed: {},
+    watch: {},
+    methods: {
+    }
+  }
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
