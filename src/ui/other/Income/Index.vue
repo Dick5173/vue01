2009a08c0af85f3c6ca0cfebfc281770d7cd50d3
zhaijian <zhaijian@51baibao.com>
2018-03-06 14:01:12 +0800

    收入列表加入自营/平台贷款

--- src/ui/other/Income/Index.vue
+++ src/ui/other/Income/Index.vue
@@ -13,9 +13,15 @@
         el-table-column(label="销售额（元）", prop="sale_total_amount")
           template(slot-scope="scope")
             el-button(type="text", @click="toOrder(scope.row)") {{scope.row.sale_total_amount | price}}
-        el-table-column(label="货款（元）", prop="sp_total_amount")
+        el-table-column(label="总货款（元）", prop="sp_total_amount")
           template(slot-scope="scope")
             div {{scope.row.sp_total_amount | price}}
+        el-table-column(label="自营贷款（元）", prop="self_sp_amount")
+          template(slot-scope="scope")
+            div {{scope.row.self_sp_amount | price}}
+        el-table-column(label="平台贷款（元）", prop="platform_sp_amount")
+          template(slot-scope="scope")
+            div {{scope.row.platform_sp_amount | price}}
         el-table-column(label="平台服务费（元）", prop="platform_fee")
           template(slot-scope="scope")
             div {{scope.row.platform_fee | price}}
@@ -23,7 +29,7 @@
           template(slot-scope="scope")
             div {{scope.row.tenant_total_amount | price}}
       div.total
-      div.bottom.txt-head(v-if="dataList.data && dataList.data.length>1") 销售总额: {{overview.sale_total_amount | friendlyPrice(false)}}，货款合计: {{overview.sp_total_amount | friendlyPrice(false)}}，平台服务费：{{overview.total_platform_fee | friendlyPrice(false)}}, 利润合计: {{overview.tenant_total_amount | friendlyPrice(false)}}
+      div.bottom.txt-head(v-if="dataList.data && dataList.data.length>1") 销售总额: {{overview.sale_total_amount | friendlyPrice(true)}}，总贷款: {{overview.sp_total_amount | friendlyPrice(true)}}，自营货款: {{overview.self_sp_amount | friendlyPrice(true)}}, 平台货款: {{overview.platform_sp_amount | friendlyPrice(true)}}, 平台服务费:{{overview.total_platform_fee | friendlyPrice(true)}}, 利润: {{overview.tenant_total_amount | friendlyPrice(true)}}
       el-pagination(:currentPage="queryPager.page", :pageSize="queryPager.limit", :total="dataListTotal",  @current-change="changePage")
 </template>
 
