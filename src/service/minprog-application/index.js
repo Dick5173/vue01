baf4ea511d2d4c0338c9c56687fc11d44f2ba89e
wangyang <superyfwy@gmail.com>
2018-03-27 18:29:05 +0800

    小程序申请， 显示小程序

--- src/service/minprog-application/index.js
+++ src/service/minprog-application/index.js
@@ -2,9 +2,14 @@ export const allPlatform = {
   pc: {
     value: 1,
     text: 'PC'
+  },
+  micro_prog: {
+    value: 2,
+    text: '小程序'
   }
 }
 
 export const allPlatformMap = {
-  1: 'PC'
+  1: 'PC',
+  2: '小程序'
 }
