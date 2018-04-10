12d90d2c76180fd9f0c392adf5977385aaa6f623
zhaijian <zhaijian@51baibao.com>
2018-04-10 13:45:31 +0800

    “联系我们”接口添加平台类型

--- src/service/minprog-application/index.js
+++ src/service/minprog-application/index.js
@@ -3,13 +3,18 @@ export const allPlatform = {
     value: 1,
     text: 'PC'
   },
-  micro_prog: {
+  bm_micro_prog: {
     value: 2,
-    text: '小程序'
+    text: '包妈商场'
+  },
+  meishi_micro_prog: {
+    value: 3,
+    text: '美市小程序'
   }
 }
 
 export const allPlatformMap = {
   1: 'PC',
-  2: '小程序'
+  2: '包妈商场',
+  3: '美市小程序'
 }
