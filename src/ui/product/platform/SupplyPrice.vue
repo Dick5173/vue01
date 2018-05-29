4f3be7de7b3c87e2e081a916d15dcefe39382934
liuyu <lewis.l.yue@gmail.com>
2018-05-29 17:05:34 +0800

    调整商品供货价 显示中级店铺

--- src/ui/product/platform/SupplyPrice.vue
+++ src/ui/product/platform/SupplyPrice.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   div
-    el-table.small-el-table(:data="filterDataList", border, :show-header="false")
+    el-table.small-el-table(:data="dataList", border, :show-header="false")
       el-table-column(label="等级" )
         template(slot-scope="props")
           div {{props.row.tenant_level.name}}
@@ -18,39 +18,25 @@
     props: {
       dataList: {
         type: Array,
-        required: true
+        default () {
+          return []
+        }
       },
       dataPrice: String,
       skus: {
         type: Array,
-        required: true
+        default () {
+          return []
+        }
       }
     },
     data () {
       return {
-        newData: [],
-        formRules: {
-          supply_price: [
-            // {required: true, message: '不能为空', trigger: 'blur'},
-            // {validator: priceValidator, trigger: 'blur'}
-          ]
-        }
-      }
-    },
-    computed: {
-      filterDataList () {
-        let newData = []
-        for (var i = 0; i < this.dataList.length; i++) {
-          if (this.dataList[i].tenant_level.name !== '中级店铺') {
-            newData.push(this.dataList[i])
-          }
-        }
-        return newData
       }
     },
     methods: {
       supplyPrice (price, row) {
-        return ProductSer.supplyPrice(price, this.skus, row.tenant_level.name === '普通店铺')
+        return ProductSer.supplyPrice(price, this.skus, row.tenant_level.name)
       }
     }
   }
