8511b8192d233faadbdbd2443d74176b8be5f88a
mawenzhuan <15831658942@163.com>
2018-06-01 13:57:47 +0800

    用户列表底部添加收入总额

--- src/ui/other/user/Index.vue
+++ src/ui/other/user/Index.vue
@@ -29,6 +29,7 @@
         el-table-column(prop="",label="操作")
           div.control-wrapper(slot-scope="scope")
             el-button.btn.text-code(size="mini", type="primary", v-if="scope.row.tenant.allow_withdraw", @click="showWithDrawDialog(scope.row)", plain) 核销提现
+      div.order-bottom.txt-head(v-if="dataList.data && dataList.data.length>0") 收入{{dataList.stat_data.sum_total_income | price}}，余额{{dataList.stat_data.sum_available_balance | price}}，核销提现{{dataList.stat_data.sum_total_withdraw | price}}
       el-pagination(:currentPage="queryPager.page", :pageSize="queryPager.limit", :total="dataListTotal",  @current-change="changePage")
       with-draw-dialog(ref="withDrawDilog")
 </template>
