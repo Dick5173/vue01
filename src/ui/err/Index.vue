46f35a7be1673ae5b5addf22a27c082a7b365ebb
liuyu <lewis.l.yue@gmail.com>
2018-06-27 23:10:04 +0800

    添加404页面

---
+++ src/ui/err/Index.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+  div
+    div 404 页面没找到
+</template>
+
+<script>
+  export default {
+    components: {},
+    data () {
+      return {}
+    },
+    computed: {},
+    methods: {}
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
